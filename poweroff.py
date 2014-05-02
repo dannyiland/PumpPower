ce964969987263a016307caed9b3e59abc3278bc
Danny Iland Kitchen PC <dannyiland@gmail.com>
2014-05-02 11:31:21 +0000

    Initial scripts to turn pump on and off

---
+++ poweroff.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11, GPIO.OUT)
+GPIO.output(11, True)
---
+++ poweron.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+GPIO.output(11, False)
